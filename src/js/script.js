e84abc744a667f9be144f85ecfe72d2d4d8d854a
Kron <brutalkron@gmail.com>
2020-10-03 20:37:23 +0300

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,37 @@
-$(document).ready(function(){
-    $('.carousel__slider').slick( 
-        {
-            
-          speed: 900,
-          // adaptiveHeight: true,
-          prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-          nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
-          
-          responsive: [
-              {
-                breakpoint: 880,
-                settings: {
-                  
-                  dots: false, 
-                  arrows: false
-                }
-              }
-          ]
-            
 
-        }
-      );
+//slider
+
+    $(document).ready(function(){
+        $('.carousel__slider').slick( 
+            {
+                
+            speed: 900,
+            // adaptiveHeight: true,
+            prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+            
+            responsive: [
+                {
+                    breakpoint: 880,
+                    settings: {
+                    
+                    dots: false, 
+                    arrows: false
+                    }
+                }
+            ]
+                
+
+            }
+        );
+
+//tabs
+
     
-      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-      $(this)
-        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     }); 
     
      
@@ -38,10 +44,25 @@ $(document).ready(function(){
             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
           });
         });  
-    };
+    }
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-});
+    });
 
+//modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
